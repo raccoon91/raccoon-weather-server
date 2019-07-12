15d05216f097536ca84a1b7d0d53abc2483f901e
raccoon <qsx314@gmail.com>
2019-07-12 18:34:03 +0900

    cron time test

--- cronjob/index.js
+++ cronjob/index.js
@@ -4,9 +4,9 @@ const shortForecast = require("../scripts/contents/shortForecast.js");
 const midForecast = require("../scripts/contents/midForecast.js");
 
 const jobList = [
-  { func: weather, cron: "00 00,10,20,30,40,50 * * * *" },
-  { func: shortForecast, cron: "20 45 * * * *" },
-  { func: midForecast, cron: "40 10 02,05,08,11,14,17,20,23 * * *" }
+  { func: weather, cron: "00,10,20,30,40,50 * * * * *" },
+  { func: shortForecast, cron: "04,14,24,34,44,54 * * * * *" }
+  // { func: midForecast, cron: "07,17,27,37,47,57 * * * *" }
 ];
 
 const cronjob = () => {
