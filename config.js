01841a9a6b815495849177f6d67626dc16d81613
raccoon <qsx314@gmail.com>
2019-07-12 17:18:17 +0900

    config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  WEATHER_KEY: process.env.WEATHER_KEY,
+
+  MYSQL_ENDPOINT:
+    process.env.MYSQL_ENDPOINT || "localhost:3306:raccoon:root:password",
+
+  REDIS_HOST: process.env.REDIS_HOST || "localhost",
+  REDIS_PORT: process.env.REDIS_PORT || "6379",
+  REDIS_PASSWORD: process.env.REDIS_PASS || "password"
+};
