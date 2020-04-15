d523828bb1b2b3c74d66f4f002a824682b888d69
raccoon91 <qsx314@gmail.com>
2020-04-15 17:01:47 +0900

    connect mysql & run cronjob

--- app.ts
+++ app.ts
@@ -3,20 +3,16 @@ import path from "path";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-// import cronjob from "./cronjob";
-// import { sequelizeConnect } from "./infra/mysql";
+import cronjob from "./cronjob";
+import { sequelizeConnect } from "./infra/mysql";
 // import { connectRedis } from "./infra/redis";
-
 import geo from "./middleware/geolocation";
-
-import config from "./config";
-
 // const indexRouter = require("./routes/index");
 
 const app = express();
 
-// // sequelize connect & execute cron cronjob
-// sequelizeConnect(cronjob);
+// sequelize connect & execute cron cronjob
+sequelizeConnect(cronjob);
 
 // // connect redis
 // connectRedis();
@@ -31,7 +27,7 @@ app.use(geo);
 
 // app.use("/", indexRouter);
 app.get("/", (req, res) => {
-	res.send(`Raccoon Weather Server ${config.ENVIRONMENT} location - ${JSON.stringify(req.body.location)}`);
+	res.send(`Raccoon Weather Server location - ${JSON.stringify(req.body.location)}`);
 });
 
 export default app;
