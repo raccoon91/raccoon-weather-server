03c1bb9403d6186758810533bc7141d1113782b8
raccoon91 <qsx314@gmail.com>
2020-04-18 18:32:51 +0900

    change testJobList to jobList

--- cronjob/index.ts
+++ cronjob/index.ts
@@ -5,24 +5,24 @@ import midForecast from "./midForecast";
 import airpollution from "./airpollution";
 import airForecast from "./airForecast";
 
-// const jobList = [
-// 	{ func: weather, cron: "40 00,10,20,30,40,50 * * * *" },
-// 	{ func: shortForecast, cron: "30 45 * * * *" },
-// 	{ func: midForecast, cron: "00 10 02,05,08,11,14,17,20,23 * * *" },
-// 	{ func: airpollution, cron: "00 10 * * * *" },
-// 	{ func: airForecast, cron: "00 00 05,11,17,23 * * *" },
-// ];
-
-const testList = [
-	{ func: weather, cron: "*/10 * * * * *" },
-	{ func: shortForecast, cron: "*/5 * * * * *" },
-	{ func: midForecast, cron: "*/5 * * * * *" },
-	// { func: airpollution, cron: "*/5 * * * * *" },
-	// { func: airForecast, cron: "*/5 * * * * *" },
+const jobList = [
+	{ func: weather, cron: "40 00,10,20,30,40,50 * * * *" },
+	{ func: shortForecast, cron: "30 45 * * * *" },
+	{ func: midForecast, cron: "00 10 02,05,08,11,14,17,20,23 * * *" },
+	{ func: airpollution, cron: "00 10 * * * *" },
+	{ func: airForecast, cron: "00 00 05,11,17,23 * * *" },
 ];
 
+// const testList = [
+// 	{ func: weather, cron: "*/10 * * * * *" },
+// 	{ func: shortForecast, cron: "*/5 * * * * *" },
+// 	{ func: midForecast, cron: "*/5 * * * * *" },
+// { func: airpollution, cron: "*/5 * * * * *" },
+// { func: airForecast, cron: "*/5 * * * * *" },
+// ];
+
 const cronjob = (): void => {
-	testList.forEach((job) => {
+	jobList.forEach((job) => {
 		new CronJob(job.cron, () => job.func(), null, false, "Asia/Seoul").start();
 	});
 };
