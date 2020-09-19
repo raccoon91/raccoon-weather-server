ac9ac13b5705e105b7bc6a0a53b81db669b83bef
raccoon91 <qsx314@gmail.com>
2020-09-19 21:20:52 +0900

    combine shor, mid controller

--- src/controllers/ForecastController.ts
+++ src/controllers/ForecastController.ts
@@ -1,29 +1,27 @@
 import { Response, Request } from "express";
+import { IForecastRouteResponse } from "../interface";
 import { ForecastService } from "../services";
 
 class ForecastController {
-  getShortForecast = async (req: Request, res: Response): Promise<Response> => {
+  getForecast = async (req: Request, res: Response): Promise<Response> => {
     const { city } = req.body.location;
+    const { term } = req.params;
 
-    const forecastData = await ForecastService.getForecast(city, true);
+    let forecastData: IForecastRouteResponse;
+
+    if (term === "short") {
+      forecastData = await ForecastService.getForecast(city, true);
+    } else if (term === "mid") {
+      forecastData = await ForecastService.getForecast(city);
+    } else {
+      return res.status(400).send("Bad Request");
+    }
 
     if (!forecastData) {
       return res.send({ message: "data not found" });
     }
 
     return res.json(forecastData);
-  };
-
-  getMidForecast = async (req: Request, res: Response): Promise<Response> => {
-    const { city } = req.body.location;
-
-    const tomorrowData = await ForecastService.getForecast(city);
-
-    if (!tomorrowData) {
-      return res.send({ message: "data not found" });
-    }
-
-    return res.json(tomorrowData);
   };
 }
 
